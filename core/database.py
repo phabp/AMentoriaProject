956170f4520e4d6b597d52df9cbfedc479f6f16a
Paulo Henrique A. de Barros <paulo.henriquepereira.adv@gmail.com>
2026-05-03 13:07:46 -0300

    Feature/data base connection (#5)
    * Add docker-compose file for PostgreSQL, in order to virtualize
    
    * Add root .gitignore
    
    * Update database configuration, because previously we were using mysql
    
    * Add project dependencies

--- core/database.py
+++ core/database.py
@@ -1,26 +1,32 @@
-# core/database.py
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-# 1. A URL de conexão. Aqui usamos o SQLite para gerar um arquivo local.
-# No futuro, mudaremos para algo como "postgresql://usuario:senha@localhost/tutor_db"
-SQLALCHEMY_DATABASE_URL = "sqlite:///./tutor_banco_local.db"
+load_dotenv()
 
-# 2. Cria o "Motor" que se comunica com o banco
-# O connect_args é uma exigência específica do SQLite no FastAPI
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not SQLALCHEMY_DATABASE_URL:
+    raise ValueError("DATABASE_URL not defined in .env file")
+
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True  
 )
 
-# 3. Cria a fábrica de sessões (as conversas do nosso backend com o banco)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
-# 4. A classe Base. Todos os nossos modelos de tabelas vão herdar dela.
 Base = declarative_base()
 
 def get_db():
     """
-    Cria uma sessão de banco de dados para uma requisição e a fecha logo depois.
+    This creates a session with the database and closes it right after
     """
     db = SessionLocal()
     try:
