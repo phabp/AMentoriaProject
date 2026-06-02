6867975737dc79d8de2a6cc6fd01c598304d3fed
Paulo Pereira <paulo.pereira@liferay.com>
2026-06-02 20:42:30 -0300

    vercel address on main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ app = FastAPI(title="Tutor Socrático ENEM", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+    "https://amentoria-project.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
